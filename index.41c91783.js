fc0604095db7edf08584818e1e8fb67edaada25f
Yasinetsky <100276712+Yasinetsky@users.noreply.github.com>
2023-03-14 22:07:59 +0000

    Deploying to gh-pages from @ Yasinetsky/goit-js-hw-10@8b1d556e0f3dcf8a96ee3879dc1280e36cebf455 🚀

---
+++ index.41c91783.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-card-container"),document.querySelector(".js-search-form"),fetch("https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages ").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
+//# sourceMappingURL=index.41c91783.js.map
